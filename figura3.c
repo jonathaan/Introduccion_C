a54b882ee662a08f6a64ae7bb8a2f385b8db0c55
jonathan <jona.da@hotmail.com>
2014-07-09 12:40:35 -0500

    Archivo figura3.c

---
+++ figura3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+int m=5,i,j;
+for(i=1;i<=m; i++){
+  for(j=i; j<=m;j++)
+  printf(" ");
+    for (j=1; j<=i; j++)
+     printf("*");
+    printf ("\n");
+}
+return 0;
+}
