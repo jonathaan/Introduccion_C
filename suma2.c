c182424aec7d517def13b374c9751ea2728ea25b
jonathan <jona.da@hotmail.com>
2014-07-09 12:38:56 -0500

    Archivo suma2.c

---
+++ suma2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+int a, b, c;
+printf ("valor de a:");
+scanf ("%d",& a);
+printf ("valor de b:");
+scanf ("%d",& b);
+c=a+b;
+printf ("la suma es:%d \n", c);
+return 0;
+}
